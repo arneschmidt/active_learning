6e61ff1bc58f9641dc0ba2293a9067dab764becc
arne <arne@decsai.ugr.es>
2022-07-22 15:37:13 +0200

    fix seed bug

--- src/globals.py
+++ src/globals.py
@@ -67,8 +67,8 @@ def set_seeds(seed=0):
 def set_global_determinism(seed=0):
     set_seeds(seed=seed)
 
-    os.environ['TF_DETERMINISTIC_OPS'] = '1'
-    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+    # os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    # os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
 
     tf.config.threading.set_inter_op_parallelism_threads(1)
     tf.config.threading.set_intra_op_parallelism_threads(1)
