af5d88713ccd4f075e4b9b1439549f9b0170f409
arne <arne@decsai.ugr.es>
2022-07-22 15:54:31 +0200

    add experiments

---
+++ dataset_dependent/panda/experiments/ablation_studies/run3.sh
@@ -0,0 +1,9 @@
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_1_0_ood_2_0_s0
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_1_0_ood_2_0_s1
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_1_0_ood_2_0_s2
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_2_0_ood_0_5_s0
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_2_0_ood_0_5_s1
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_2_0_ood_0_5_s2
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_2_0_ood_1_0_s0
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_2_0_ood_1_0_s1
+python src/main.py -dc ../../experiments/panda/experiments/ablation_studies/config.yaml -ef ../../experiments/panda/experiments/ablation_studies/al_2_0_ood_1_0_s2
---
+++ dataset_dependent/panda/experiments/acquisition/run.sh
@@ -0,0 +1,3 @@
+python src/main.py -dc ../../experiments/panda/experiments/acquisition/config.yaml -ef ../../experiments/panda/experiments/acquisition/ac_new_ood_normalization
+python src/main.py -dc ../../experiments/panda/experiments/acquisition/config.yaml -ef ../../experiments/panda/experiments/acquisition/ac_new_ood_normalization_seed1
+python src/main.py -dc ../../experiments/panda/experiments/acquisition/config.yaml -ef ../../experiments/panda/experiments/acquisition/ac_new_ood_normalization_seed2
